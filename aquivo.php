ca873bc1188ef15faadb257d5acc65772daa8583
João Manuel de Souza Marques <93559636+Joao-Manuel-S-M@users.noreply.github.com>
2022-05-02 20:00:50 -0300

    Update aquivo.php

--- aquivo.php
+++ aquivo.php
@@ -5,8 +5,7 @@
      $mensagem = $_POST['Mensagem'];
      $data_envio = date('d/m/Y');
      $hora_envio = date('H:i:s');
-   
-     //Compo E-mail
+ 
      $arquivo = "
        <html>
          <p><b>Nome: </b>$nome</p>
@@ -16,16 +15,13 @@
        </html>
      ";
      
-     //Emails para quem será enviado o formulário
      $destino = "jmsmarques8213@gmail.com";
      $assunto = "Teste";
    
-     //Este sempre deverá existir para garantir a exibição correta dos caracteres
      $headers  = "MIME-Version: 1.0\n";
      $headers .= "Content-type: text/html; charset=iso-8859-1\n";
      $headers .= "From: $nome <$email>";
    
-     //Enviar
      mail($destino, $assunto, $arquivo, $headers);
      
      # echo "<meta http-equiv='refresh' content='10;URL=../contato.html'>";
